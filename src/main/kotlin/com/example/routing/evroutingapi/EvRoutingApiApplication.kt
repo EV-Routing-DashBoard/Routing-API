d1e9543b1d2abe0484585a69086c1d32e3e192c0
MustafaSarikaya <msarikay@uottawa.ca>
2024-03-02 21:52:29 -0400

    initialize the project

---
+++ src/main/kotlin/com/example/routing/evroutingapi/EvRoutingApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.routing.evroutingapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EvRoutingApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<EvRoutingApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/routing/evroutingapi/EvRoutingApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.routing.evroutingapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EvRoutingApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
